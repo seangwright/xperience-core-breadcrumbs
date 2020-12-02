0a61a098e8032a840b15d4113d2d75aaddbf06d3
Eric Dugre <EricD@kentico.com>
2020-12-02 11:51:44 -0500

    Add viewmodel

---
+++ Components/Widgets/BreadcrumbsWidget/BreadcrumbsWidgetViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Lucene.Net.Documents;
+using System.Collections.Generic;
+
+namespace Xperience.Components.Widgets.BreadcrumbsWidget
+{
+    public class BreadcrumbsWidgetViewModel
+    {
+        public IEnumerable<BreadcrumbItem> Hierarchy { get; set; }
+        public string Separator { get; set; }
+        public string ClassName { get; set; }
+    }
+}
